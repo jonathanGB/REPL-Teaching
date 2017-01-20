06fdc57c987a65dd70849f8c6585d0257d4345d5
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2017-01-20 17:58:03 -0500

    Amélioration de la maquette du login

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//window.onload = () => alert('wazza')
+$(function() {
+    // at the beginning
+    updateFooterOpacity()
+
+
+    // events
+    $(window).scroll(updateFooterOpacity)
+})
+
+
+// functions
+function updateFooterOpacity() {
+    if($(window).scrollTop() + $(window).height() >= $(document).height() - 50) {
+        $('footer').css('opacity', 1)
+    } else {
+        $('footer').css('opacity', 0.7)
+    }
+}
