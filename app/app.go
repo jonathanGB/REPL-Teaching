510402f814d4ee8df39d48bc10d79ab5710e552d
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2017-02-01 17:12:12 -0500

    mongodb connecté avec le serveur, endpoint /foo de test

--- app/app.go
+++ app/app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"gopkg.in/gin-gonic/gin.v1"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"net/http"
 )
 
@@ -9,6 +11,9 @@ func main() {
 	router := gin.Default()
 	router.Static("/public", "./public")
 	router.StaticFile("/favicon.ico", "./public/img/favicon.ico")
+
+	s := initDB()
+	defer s.Close()
 
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World!")
@@ -18,5 +23,24 @@ func main() {
 		c.String(http.StatusOK, "pong")
 	})
 
+	router.GET("/foo", func(c *gin.Context) {
+		fooObj := struct {
+			Id  bson.ObjectId `json:"id" bson:"_id"`
+			Kek bool          `json:"topkek" bson:"kek"`
+		}{}
+
+		s.DB("repl").C("foo").Find(bson.M{}).One(&fooObj)
+		c.JSON(http.StatusOK, fooObj)
+	})
+
 	router.Run(":8080")
+}
+
+func initDB() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost")
+	if err != nil {
+		panic(err)
+	}
+
+	return s
 }
