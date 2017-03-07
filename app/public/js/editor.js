27269df03945d68c1d5a9c5d45a68f9a37fe8389
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2017-03-06 23:22:52 -0500

    éditeur a des droits limités si pas le owner

--- app/public/js/editor.js
+++ app/public/js/editor.js
@@ -7,13 +7,16 @@ $(function() {
 		go: 'golang'
 	}
 	let wsURL =`ws://${location.host}${location.pathname}ws`
+	let isOwner = editorElem.data('isowner')
 
   editor.setTheme("ace/theme/monokai");
   editor.setHighlightActiveLine(true);
   editor.session.setMode(`ace/mode/${extensionLangMap[editorElem.data('extension')]}`);
   editor.setShowPrintMargin(false);
   editor.setFontSize(14);
-	editor.setValue(atob(editorElem.data('code')))
+	editor.setValue(atob(editorElem.data('code')), -1)
+	editor.setReadOnly(!isOwner)
+
 	// TODO: set read-only if not owner of file
 
   let results = ace.edit("results");
@@ -23,38 +26,39 @@ $(function() {
 
 	$('#changeStatus').click(function(e) {
 		let currStatus = $(this).data('status')
-		// update db
+		// TODO: update db
 		// change style
 		$(this).data('status', !currStatus)
 		$(this).children('.status-text').text(currStatus ? "Public" : "Privé").siblings('.file-status').removeClass(`${currStatus}`).addClass(`${!currStatus}`)
 	})
 
+	if (isOwner) {
+		const socket = new WebSocket(wsURL)
+		socket.onopen = () => {
+			console.log('connected')
 
-	const socket = new WebSocket(wsURL)
-	socket.onopen = () => {
-		console.log('connected')
+			$('#saveFile').click(function(e) {
+				let data = editor.getValue()
+				if (data.length > MAX_FILE_SIZE) {
+					toastr.error("Le fichier dépasse la limite de 10kB")
+					return
+				}
 
-		$('#saveFile').click(function(e) {
-			let data = editor.getValue()
-			if (data.length > MAX_FILE_SIZE) {
-				toastr.error("Le fichier dépasse la limite de 10kB")
-				return
+				let toSend = {
+					type: "update-file",
+					data
+				}
+				socket.send(JSON.stringify(toSend))
+			})
+
+			socket.onmessage = (e) => {
+				console.log('message', e)
+				editor.setValue(JSON.parse(e.data).data.substring(0, 500))
 			}
 
-			let toSend = {
-				type: "update-file",
-				data
+			socket.onclose = (e) => {
+				console.log('close', e)
 			}
-			socket.send(JSON.stringify(toSend))
-		})
-
-		socket.onmessage = (e) => {
-			console.log('message', e)
-			editor.setValue(JSON.parse(e.data).data.substring(0, 500))
-		}
-
-		socket.onclose = (e) => {
-			console.log('close', e)
 		}
 	}
 
