42a70be1e4b4d26a725c05f9ad99adef2579984b
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2017-03-21 09:04:39 -0400

    ajouté script pour installer les images docker manquantes, greffé au script de déploiement

---
+++ app/buildDockerImages.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+declare -A runImages=( ["run-js"]="false" ["run-go"]="false" ["run-rb"]="false" ["run-py"]="false" ["run-exs"]="false" )
+
+buildDockerImages() {
+	builtImages=$(docker images | awk '$1 ~ /^run-/{print $1}')
+}
+exists() {
+	if [ -v runImages[$1] ]; then
+		runImages[$1]="true"
+	fi
+}
+buildDockerImages
+
+
+while read -r image; do
+	exists $image
+done <<< "$builtImages"
+
+for i in "${!runImages[@]}"
+do
+	if [ ${runImages[$i]} == "false" ]; then
+		echo "Building $i docker image..."
+		echo ""
+		docker build --no-cache -f "./services/run/images/$i.dockerfile" -t "$i" "./services/run/images/"
+		echo ""
+		echo ""
+	fi
+done
--- app/deploy.bash
+++ app/deploy.bash
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Before using this, two things are assumed!
 # 1. You have Go installed, and have set your GOPATH
 # 2. You have MongoDB installed, and have set the default path to data (/data/db)
 
-sudo mongod > /dev/null &
+buildDockerImagesFlag="true"
+
+mongod > /dev/null &
 
 for var in "$@"
 do
@@ -16,7 +18,11 @@ do
 		echo "Installing bson"   && go get gopkg.in/mgo.v2/bson &&
 		echo "Installing bcrypt" && go get golang.org/x/crypto/bcrypt &&
 		echo "Installing jwt" && go get github.com/dgrijalva/jwt-go &&
-		echo "Installing gorilla-toolkit/websocket" && go get github.com/gorilla/websocket
+		echo "Installing gorilla-toolkit/websocket" && go get github.com/gorilla/websocket &&
+		echo "Installing docker/api/types" && go get "github.com/docker/docker/api/types" &&
+		echo "Installing docker/api/types/container" && go get "github.com/docker/docker/api/types/container" &&
+		echo "Installing docker/client" && go get "github.com/docker/docker/client" &&
+		echo ""
 	fi
 
 	# to restart the database (creates by default a "foo" collection)
@@ -26,7 +32,16 @@ do
 		#mongo repl --eval 'db.createCollection("users"); db.users.insert([{ "_id" : ObjectId("58ae488a9d1fa299c7dc795a"), "name" : "topprof", "email" : "foo@bar", "role" : "teacher", "groups" : [ObjectId("58ae4b00462a70ed0c38367d") ], "password" : BinData(0,"JDJhJDEwJENwYmx3elNiWGhiVlpFWVZyUFdlY090VHJuQW0vbjdVc1pDMFpuQUpJQWhoaWQ0RHV1WXUy") }, { "_id" : ObjectId("58ae48e39d1fa299c7dc795b"), "name" : "topstudent", "email" : "student@bar", "role" : "student", "groups" : [ ObjectId("58ae4b00462a70ed0c38367d") ], "password" : BinData(0,"JDJhJDEwJGVqMk5Nd0JXbUJhRDlCalpBdXR5dnVnZmU5NlN0LkZQY0J3MS5VUlNRVFppWW9DLjhGOHhL") }]);' &&
 		#mongo repl --eval 'db.createCollection("groups"); db.groups.insert({"_id": ObjectId("58ae4b00462a70ed0c38367d"), name: "fou groupe", teacher: ObjectId("58ae488a9d1fa299c7dc795a"), "teacher-name": "topprof", files: [], password: BinData(0,"JDJhJDEwJENwYmx3elNiWGhiVlpFWVZyUFdlY090VHJuQW0vbjdVc1pDMFpuQUpJQWhoaWQ0RHV1WXUy")});'
 	fi
+
+	#cancel docker Images build if not found default behaviour
+	if [ "$var" = "--noBuild" ]; then
+		buildDockerImagesFlag="false"
+	fi
 done
+
+if [ "$buildDockerImagesFlag" == "true" ]; then
+	./buildDockerImages.bash
+fi
 
 
 go run app.go
